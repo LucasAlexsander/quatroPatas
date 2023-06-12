c8c0a849b31fe168358b1870ed22f7105fe4fa54
Lucas Alexsander Barbosa Cruz <lucas10barbosa_@hotmail.com>
2023-06-12 12:43:42 -0300

    First Commit

---
+++ assets/js/script.js
@@ -0,0 +1,30 @@
+window.addEventListener("load", function () {
+  new Glider(document.querySelector(".carrossel"), {
+    slidesToShow: 1,
+    dots: ".dots",
+    draggable: true,
+    arrows: {
+      prev: ".glider-prev",
+      next: ".glider-next",
+    },
+  });
+});
+
+// FAQ
+const areaPerguntas = document.querySelectorAll(".caixa-pergunta");
+areaPerguntas.forEach((caixa) => {
+  const button = caixa.children[0];
+  console.log();
+  button.addEventListener("click", () => {
+    if (caixa.classList.contains("active")) {
+      caixa.classList.remove("active");
+      return true;
+    }
+    // Tirar classe active
+    areaPerguntas.forEach((e) => {
+      e.classList.remove("active");
+    });
+    // Adionando classe active
+    caixa.classList.add("active");
+  });
+});
